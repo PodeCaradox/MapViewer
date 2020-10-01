694cd775af652cc1ca2ad88510ba65e6ee514c90
Gaaammmler <Gaaammmler@DESKTOP-SSLM1K5>
2020-10-01 21:09:41 +0200

    added camera

---
+++ GodotMapViewer/skripts/camera.gd
@@ -0,0 +1,28 @@
+extends Camera2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func zoom():
+	position.x += 1
+	
+	if Input.is_action_just_released('wheel_down'):
+		zoom.x += 0.25
+		zoom.y += 0.25
+	if Input.is_action_just_released('wheel_up') and zoom.x > 1 and zoom.y > 1:
+		zoom.x -= 0.25
+		zoom.y -= 0.25
+
+func _physics_process(delta):
+	zoom()
